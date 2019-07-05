eab34eb92957c16764e050602eab5c7c121831c9
Felipe Marinho <felipmarinho@gmail.com>
2019-07-05 05:09:40 -0300

    #3 - Adding authentication and a middleware

---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: 'cd0f5a12d3a2f4bcfc6ab0e181757075',
+  expiresIn: '7d',
+};
--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,16 @@
 import { Router } from 'express';
 
 import UserController from './app/controller/UserController';
+import authMiddleware from './app/middlewares/auth';
+import SessionController from './app/controller/SessionController';
 
 const routes = new Router();
 
+routes.post('/users', UserController.store);
+routes.post('/sessions', SessionController.store);
+
+routes.use(authMiddleware);
 routes.get('/users', UserController.get);
 routes.put('/users', UserController.update);
-routes.post('/users', UserController.store);
 
 export default routes;
