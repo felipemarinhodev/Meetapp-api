199a95e763df893b0014e69c43a8a13a2c3411c3
Felipe Marinho <felipmarinho@gmail.com>
2019-07-10 06:29:41 -0300

    Adding and configuring multer and create upload file

--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,21 @@
 import { Router } from 'express';
 
-import UserController from './app/controller/UserController';
+import multer from 'multer';
+import multerConfig from './config/multer';
+
 import authMiddleware from './app/middlewares/auth';
+
+import UserController from './app/controller/UserController';
+import FileController from './app/controller/FileController';
 import SessionController from './app/controller/SessionController';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
 
+routes.post('/files', upload.single('file'), FileController.store);
 routes.use(authMiddleware);
 routes.get('/users', UserController.get);
 routes.put('/users', UserController.update);
