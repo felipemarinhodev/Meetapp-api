e7e8779d2e04c9d923834152ebd0888b1e8f02fd
Felipe Marinho <felipmarinho@gmail.com>
2019-07-12 06:35:30 -0300

    #8 - Creating migration for subscribing to a meeting

---
+++ src/database/migrations/20190712075926-create-subscribe.js
@@ -0,0 +1,42 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('subscribes', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      subscribe_in: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      user_id: {
+        type: Sequelize.INTEGER,
+        references: { model: 'users', key: 'id' },
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL',
+        allowNull: true,
+      },
+      meetup_id: {
+        type: Sequelize.INTEGER,
+        references: { model: 'meetups', key: 'id' },
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL',
+        allowNull: true,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+  },
+
+  down: queryInterface => {
+    return queryInterface.dropTable('subscribes');
+  },
+};
